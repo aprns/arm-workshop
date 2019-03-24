4a2089d1a534b1b1a5016ba33d94075ffbebd0e1
Erikas Seselskis <erikas.seselskis@gmail.com>
2019-03-24 20:27:36 +0200

    Lab3

---
+++ ps.ps1
@@ -0,0 +1,3 @@
+﻿az group deployment create --name job1 --resource-group lab2 --template-file azuredeploy.json --parameters "@azuredeploy.parameters.json" --output json
+Get-AzResourceGroup
+$vm = New-AzResourceGroupDeployment -Name job1 -ResourceGroupName lab2 -TemplateFile azuredeploy.json -TemplateParameterFile azuredeploy.parameters.json
